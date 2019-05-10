b706cb7030975ac12b1cf50212dbdcf246fb714b
xiaofan <luoxiaofan.xiong@gmail.com>
2019-05-10 09:20:56 +0200

    RentrerPoint + correction constructeur Point
    (cherry picked from commit 92f8669148ca4341c9dde43729cc9dc7a7c45775)

--- Input.cpp
+++ Input.cpp
@@ -96,6 +96,45 @@ double Input::rentrerRayon()
 
 tuple<Date, Date> Input::rentrerDebutFin()
 {
+    Date debut;
+    Date fin;
+    string annee1;
+    string mois1;
+    string jour1;
+    string heure1;
+    string minute1;
+    string seconde1;
+    string annee2;
+    string mois2;
+    string jour2;
+    string heure2;
+    string minute2;
+    string seconde2;
+    string milliseconde = 0;
+    bool conversionReussie = true;
+
+    do {
+        cout << "Veuillez rentrez la date début de la recherche: (exemple: 2000 01 01 00 00 00)" << endl;
+        cin >> annee1 >> mois1 >> jour1 >> heure1 >> minute1 >> seconde1;
+        cout << "Veuillez rentrez la date fin de la recherche: (exemple: 2000 01 01 00 00 00)" << endl;
+        cin >> annee2>> mois2 >> jour2 >> heure2 >> minute2 >> seconde2;
+        try {
+            debut = Date(annee1,mois1,jour1,heure1,minute1,seconde1,milliseconde);
+            fin = Date(annee1,mois1,jour1,heure1,minute1,seconde1,milliseconde);
+        }
+        catch (const std::invalid_argument&) {
+            conversionReussie = false;
+        }
+        catch (const std::out_of_range&) {
+            conversionReussie = false;
+        }
+        if(debut>fin){
+        cout << "Date début et date fin incohérente" << endl;
+        }else if (!conversionReussie) {
+            cout << "Rayon incorrecte. Réessayez." << endl;
+        }
+    } while (!conversionReussie);
+
     return tuple<Date, Date>();
 }
 
