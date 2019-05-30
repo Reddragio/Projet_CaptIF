d9467cc72055b1c2c8f8740d679dee0c16e79c0a
Reddragio <jacques.r.charnay@hotmail.fr>
2019-05-30 19:57:18 +0200

    Installation Google Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "Controleur/CaptIF.h"
-#include "Modele/RequestView.h"
+#include "CaptIF/Controleur/CaptIF.h"
+#include "CaptIF/Modele/RequestView.h"
 #include <ctime>
 
 int main() {
@@ -18,25 +18,6 @@ int main() {
     duree = (clock() - start) / (double)CLOCKS_PER_SEC;
     cout << "Initialisation des Sensors et Attributs en "<< duree << "s" << endl;
     cout << endl;
-
-    /*
-    //Test de la RequestView
-    set<string> sensorIds;
-    sensorIds.insert("Sensor7");
-    sensorIds.insert("Sensor8");
-
-    //Debut chrono
-    start = clock();
-    //Test en lui même
-    RequestView rtest(fichiers,sensorIds,Date(1980,0,0,0,0,0,0),Date(2050,0,0,0,0,0,0));
-    vector<Measure> res;
-    while(rtest.goToNext()){
-        res.push_back(rtest.getMeasure());
-    }
-    //fin chrono
-    duree = (clock() - start) / (double)CLOCKS_PER_SEC;
-    cout << "Nombre de mesures retenues: "<<res.size() << endl;
-    cout << "En: "<<duree << "s"<<endl;*/
 
     application.run();
     return 0;
