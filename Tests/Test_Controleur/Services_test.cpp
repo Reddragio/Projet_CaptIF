f87a423e65a18e89c8105933842343c24088d04f
xiaofan <luoxiaofan.xiong@gmail.com>
2019-06-01 21:45:28 +0200

    test qualiteAirPointPeriode & test qualiteAirPointMoment

--- Tests/Test_Controleur/Services_test.cpp
+++ Tests/Test_Controleur/Services_test.cpp
@@ -101,3 +101,40 @@ TEST_F(Services_test,testQualiteAirTerritoireMoment)
     ASSERT_EQ(get<1>(res["O3"]),(59.5323533239709+68.7643773753426)/2.0); /* changez les valeurs qu'on doit trouver */
     ASSERT_EQ(get<1>(res["PM10"]),(6.33424404998439+10.9832137846515)/2.0);
 }
+
+TEST_F(Services_test,testQualiteAirPointPeriode){
+    /*2017-01-01T00:01:20.6090000;Sensor0;O3;17.8902017543936;
+    2017-01-01T00:01:20.6090000;Sensor0;NO2;42.4807462361763;
+    2017-01-01T00:01:20.6090000;Sensor0;SO2;13.6449094925285;
+    2017-01-01T00:01:20.6090000;Sensor0;PM10;1.55796479844986;
+    2017-01-01T00:30:39.0040000;Sensor0;O3;36.7797600526823;
+    2017-01-01T00:30:39.0040000;Sensor0;NO2;80.2280346451481;
+    2017-01-01T00:30:39.0040000;Sensor0;SO2;38.151540049253;
+    2017-01-01T00:30:39.0040000;Sensor0;PM10;1.99603267330184;*/
+
+    Point p1(0.0,0.0);
+    //Date(int year, int month,int day,int hour, int min, int sec,int msecInit);
+    Date debut(2017,1,8,0,1,20,0);
+    Date fin(2017,1,8,0,32,0,0);
+    map<string,tuple<int, double, int>> res = services.qualiteAirPointPeriode(p1,debut,fin);
+    ASSERT_EQ(get<1>(res["O3"]),(17.8902017543936+36.7797600526823)/2.0);
+    ASSERT_EQ(get<1>(res["PM10"]),(1.55796479844986+1.99603267330184)/2.0);
+}
+
+TEST_F(Services_test,testQualiteAirPointMoment){
+    /*2017-01-01T00:01:20.6090000;Sensor0;O3;17.8902017543936;
+    2017-01-01T00:01:20.6090000;Sensor0;NO2;42.4807462361763;
+    2017-01-01T00:01:20.6090000;Sensor0;SO2;13.6449094925285;
+    2017-01-01T00:01:20.6090000;Sensor0;PM10;1.55796479844986;
+    2017-01-01T00:30:39.0040000;Sensor0;O3;36.7797600526823;
+    2017-01-01T00:30:39.0040000;Sensor0;NO2;80.2280346451481;
+    2017-01-01T00:30:39.0040000;Sensor0;SO2;38.151540049253;
+    2017-01-01T00:30:39.0040000;Sensor0;PM10;1.99603267330184;*/
+
+    Point p1(0.0,0.0);
+    //Date(int year, int month,int day,int hour, int min, int sec,int msecInit);
+    Date moment(2017,1,8,1,20,0,0);
+    map<string,tuple<int, double, int>> res = services.qualiteAirPointMoment(p1,moment);
+    ASSERT_EQ(get<1>(res["O3"]),(17.8902017543936+36.7797600526823)/2.0);
+    ASSERT_EQ(get<1>(res["PM10"]),(1.55796479844986+1.99603267330184)/2.0);
+}
