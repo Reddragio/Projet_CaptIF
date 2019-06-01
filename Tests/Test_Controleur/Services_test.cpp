3cb634fdccd1e4f0232cc387242da238f8ae8bcb
Reddragio <jacques.r.charnay@hotmail.fr>
2019-06-01 18:57:40 +0200

    Ajout test Output + valeurs Sofiane

--- Tests/Test_Controleur/Services_test.cpp
+++ Tests/Test_Controleur/Services_test.cpp
@@ -96,8 +96,8 @@ TEST_F(Services_test,testQualiteAirTerritoireMoment)
 
     Point p1(0.0,0.0);
     //Date(int year, int month,int day,int hour, int min, int sec,int msecInit);
-    Date moment(2017,1,8,0,1,20,0);
+    Date moment(2017,1,8,1,20,0,0);
     map<string,tuple<int, double, int>> res = services.qualiteAirTerritoireMoment(p1,5,moment);
-    ASSERT_EQ(get<1>(res["O3"]),(17.8902017543936+36.7797600526823)/2.0); /* changez les valeurs qu'on doit trouver */
-    ASSERT_EQ(get<1>(res["PM10"]),(1.55796479844986+1.99603267330184)/2.0);
+    ASSERT_EQ(get<1>(res["O3"]),(59.5323533239709+68.7643773753426)/2.0); /* changez les valeurs qu'on doit trouver */
+    ASSERT_EQ(get<1>(res["PM10"]),(6.33424404998439+10.9832137846515)/2.0);
 }
--- Tests/Test_Vue/Output_test.cpp
+++ Tests/Test_Vue/Output_test.cpp
@@ -45,3 +45,24 @@ TEST_F(Output_test,testBidon)
 {
     ASSERT_TRUE(true);
 }
+
+TEST_F(Output_test,testMessage)
+{
+    testing::internal::CaptureStdout();
+    output.afficherMessageAccueil(services);
+    string output = testing::internal::GetCapturedStdout();
+
+    string objectif;
+    objectif.append("\n");
+    objectif.append("Bienvenue sur l'application Capt'IF\n");
+    objectif.append("\n");
+    objectif.append("Un projet realise par:\n");
+    objectif.append("Jacques Charnay, Pierre Scheer, Sofiane Benslimane & Luoxiaofan Xiong\n");
+    objectif.append("\n");
+    objectif.append("Initialisation terminee\n");
+    objectif.append("Nombre de capteurs : 10\n");
+    objectif.append("Nombre d'attributs : 4\n");
+    objectif.append("\n");
+
+    ASSERT_EQ(output,objectif);
+}
